7b1d894fbffc6293cc5fd4752f85a4fdde173a11
Zachary Kirby <zkirby@berkeley.edu>
2020-12-13 16:13:20 -0800

    added 404

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-jsx": () => import("./../../../src/pages/404.jsx" /* webpackChunkName: "component---src-pages-404-jsx" */),
   "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
 }
 
---
+++ src/pages/404.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FourOFour = () => <div>Nothin to see here folks, go home.</div>;
+
+export default FourOFour;
